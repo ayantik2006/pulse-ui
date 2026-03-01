ee13d3bf730d5677363193a5f3e1ed901900cc2f
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-03-01 15:54:42 +0530

    loader 9 updated

--- pulseui-components/Loaders/Loader9.tsx
+++ pulseui-components/Loaders/Loader9.tsx
@@ -1,16 +1,22 @@
 import { motion } from "framer-motion";
 
-const sweepTransition = {
-  duration: 1.2,
-  ease: "easeInOut",
-  repeat: Infinity,
-};
-
 export default function Loader() {
   return (
     <div className="flex flex-col relative gap-1">
-        <motion.div className="bg-blue-300 h-1 rounded-4xl w-10 animate-pulse relative"  animate={{opacity : 2,x: ["0%","150%"] }} transition={{ repeat: Infinity, duration: 0.4, ease:"linear", }}> </motion.div>
-        <motion.div className="bg-blue-300 h-1 rounded-4xl w-10 animate-pulse relative left-4"  animate={{opacity : 2,x: ["0%","150%"] }} transition={{ repeat: Infinity, duration: 0.4,  ease: "linear", }}> </motion.div>
+      <motion.div
+        className="bg-blue-300 h-1 rounded-4xl w-10 animate-pulse relative"
+        animate={{ opacity: 2, x: ["0%", "150%","0%"] }}
+        transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+      >
+        {" "}
+      </motion.div>
+      <motion.div
+        className="bg-blue-300 h-1 rounded-4xl w-10 animate-pulse relative left-4"
+        animate={{ opacity: 2, x: ["0%", "150%","0%"] }}
+        transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+      >
+        {" "}
+      </motion.div>
     </div>
   );
 }
