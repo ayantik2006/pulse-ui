ce21ee7a3e5eb8205390e864171f7a203f89e4d5
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-03-02 00:53:06 +0530

    image upload compo updated

--- pulseui-components/File Upload/ImageUpload.tsx
+++ pulseui-components/File Upload/ImageUpload.tsx
@@ -6,6 +6,8 @@ function ImageUpload() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [files, setFiles] = useState<File[]>([]);
   const [isDragging, setIsDragging] = useState(false);
+  const [selectedImagePath,setSelectedImagePath]=useState("");
+
   return (
     <div className="flex flex-col gap-2">
       <div
@@ -53,11 +55,9 @@ function ImageUpload() {
               className="bg-neutral-100 max-w-[22rem] h-12 rounded flex items-center justify-between gap-2 px-2"
               key={index}
             >
-              {/* <Paperclip size={15} className=" stroke-neutral-600" /> */}
-              {/* <p className="overflow-clip h-7 h-7 text-neutral-500">
-                {file.name}
-              </p> */}
-              <Image src={URL.createObjectURL(file)} width="40" height="40" alt="uploaded image" className="rounded" />
+              <Image src={URL.createObjectURL(file)} width="40" height="40" alt="uploaded image" className="rounded cursor-pointer hover:scale-[1.05] duration-300" onClick={()=>{
+                setSelectedImagePath(URL.createObjectURL(file));
+              }}/>
               <X
                 size={15}
                 className="hover:stroke-red-700 cursor-pointer duration-300"
@@ -73,6 +73,13 @@ function ImageUpload() {
           ))}
         </div>
       )}
+      {selectedImagePath!=="" && <div className="fixed inset-0 w-[100vw] h-[100vh] bg-[#ffffff16] z-80 backdrop-blur-[0.3rem] flex items-center justify-center" onClick={()=>{
+        setSelectedImagePath("");
+      }}>
+        <div className="w-fit h-fit shadow-[0_0_8px_gray] rounded-md bg-white z-100">
+            <Image src={selectedImagePath} height={600} width={600} alt="image selected" className="rounded-md"/>
+        </div>
+      </div>}
     </div>
   );
 }
