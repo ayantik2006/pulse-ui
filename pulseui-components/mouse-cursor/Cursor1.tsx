068317a24859c82d4fae7fef181be5204bd0b81f
shuvam_776 <shuvamsatapathi@gmail.com>
2026-03-29 10:22:40 +0530

    Cursor Component

---
+++ pulseui-components/mouse-cursor/Cursor1.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export default function Cursor1() {
+  return (
+    <div
+      className="
+      w-6 h-6 
+      bg-gray-800
+      rounded-full 
+      mix-blend-difference
+      shadow-lg
+      "
+    />
+  )
+}
